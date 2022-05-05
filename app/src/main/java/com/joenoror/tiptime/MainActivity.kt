32e6f5d9ca5fcf33af8d4b950b1bf92b44fde5ff
Joenoror <falanhx@gmail.com>
2022-05-05 12:27:45 +0200

    Styles and themes Updated

--- app/src/main/java/com/joenoror/tiptime/MainActivity.kt
+++ app/src/main/java/com/joenoror/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.joenoror.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.joenoror.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -18,12 +22,14 @@ class MainActivity : AppCompatActivity() {
             calculateTip()
         }
 
-
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
 
     }
 
     private fun calculateTip(){
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if(cost == null || cost == 0.0){
             displayTip(0.0)
@@ -45,4 +51,16 @@ class MainActivity : AppCompatActivity() {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
 }
